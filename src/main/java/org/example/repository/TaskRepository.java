2179acbfcb0891464633aa8c3880e7537b7b4f46
rick1135 <rick1135@hotmail.com>
2026-08-28 17:14:25 -0300

    feat: repository e service

--- src/main/java/org/example/repository/TaskRepository.java
+++ src/main/java/org/example/repository/TaskRepository.java
@@ -4,8 +4,39 @@ import org.example.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public interface TaskRepository {
+public class TaskRepository {
+    private final List<Task> tarefas = new ArrayList<>();
+    private int proxId=1;
 
+    public int gerarNovoId(){
+        return proxId++;
+    }
+
+    public void salvarRebalanceando(Task novaTarefa){
+        int posInserir=tarefas.size();
+
+        for(int i=0; i<tarefas.size(); i++){
+            if(novaTarefa.getPrioridade()>tarefas.get(i).getPrioridade()){
+                posInserir=i;
+                break;
+            }
+        }
+
+        tarefas.add(posInserir, novaTarefa); //add move os elementos para direita, abrindo espaço para novo elemento
+    }
+
+    public List<Task> listarTarefas(){
+        return tarefas;
+    }
+
+    public boolean excluirPorId(int id){
+        for(int i=0; i<tarefas.size(); i++){
+            if(tarefas.get(i).getId()==id){
+                tarefas.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -1,4 +1,70 @@
 package org.example.service;
 
+import org.example.model.Task;
+import org.example.model.TaskStatus;
+import org.example.repository.TaskRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task criarTarefa(String nome, String descricao, LocalDate dataFinal, int prioridade, String categoria, TaskStatus status){
+        if(nome==null || nome.trim().isEmpty()){
+            System.out.println("Erro: informe o nome da tarefa!");
+            return null;
+        }
+
+        if(prioridade<1||prioridade>5){
+            System.out.printf("Erro: A prioridade deve ser de 1 a 5!");
+            return null;
+        }
+
+        int id = taskRepository.gerarNovoId();
+        Task novaTarefa = new Task(id, nome, descricao, dataFinal, prioridade, categoria, status);
+
+        taskRepository.salvarRebalanceando(novaTarefa);
+        return novaTarefa;
+    }
+
+    public List<Task> listarTodas(){
+        return taskRepository.listarTarefas();
+    }
+
+    public List<Task> listarPorCategoria(String categoria){
+        List<Task> tarefasFiltradas = new ArrayList<>();
+        for(Task tarefa : taskRepository.listarTarefas()){
+            if(tarefa.getCategoria().equalsIgnoreCase(categoria))
+                tarefasFiltradas.add(tarefa);
+        }
+        return tarefasFiltradas;
+    }
+
+    public List<Task> listarPorPrioridade(int prioridade){
+        List<Task> tarefasFiltradas = new ArrayList<>();
+        for(Task tarefa: taskRepository.listarTarefas()){
+            if(tarefa.getPrioridade()==prioridade)
+                tarefasFiltradas.add(tarefa);
+        }
+        return tarefasFiltradas;
+    }
+
+    public List<Task> listarPorStatus(TaskStatus status){
+        List<Task> tarefasFiltradas = new ArrayList<>();
+        for(Task tarefa:taskRepository.listarTarefas()){
+            if(tarefa.getStatus()==status)
+                tarefasFiltradas.add(tarefa);
+        }
+        return tarefasFiltradas;
+    }
+
+    public boolean excluirTarefa(int id){
+        return taskRepository.excluirPorId(id);
+    }
 }
